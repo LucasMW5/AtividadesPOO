26905a3c9ec84ad474dd51dc8b27ea4af88e1235
LucasMW5 <wachholzlucas@gmail.com>
2026-09-03 12:49:59 -0300

    Parcialmente arrumado

--- sonora-fase02/test/MusicaTest.java
+++ sonora-fase02/test/MusicaTest.java
@@ -5,8 +5,36 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import src.Musica;
-
 public class MusicaTest {
+    private static Musica musica;
+
+    @BeforeAll
+    public static void inicializaMusica() {
+        musica = new Musica("Titulo", "Artista", 120);
+    }
+
+    @Test
+    public void testCriacaoMusica() {
+        assertNotNull(musica);
+    }
+
+    @Test
+    public void testTituloInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Musica mus = new Musica("", "Artista", 150);
+        });
+    }
+
+    @Test
+    public void testDuracaoValida() {
+        assertEquals(120, musica.getDuracaoEmSegundos());
+    }
+
+    @Test
+    public void testDuracaoInvalida() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Musica mus = new Musica("Titulo", "Artista", -150);
+        });
+    }
 
 }
