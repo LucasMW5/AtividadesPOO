e188c3612402d5383a857b96d83d60c8908f94f1
LucasMW5 <wachholzlucas@gmail.com>
2026-09-03 11:50:20 -0300

    Tentando arrumar bug MusicaTest

--- sonora-fase02/test/MusicaTest.java
+++ sonora-fase02/test/MusicaTest.java
@@ -1,13 +1,13 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import sonorafase02.src.Musica;
+import src.Musica;
 
 public class MusicaTest {
 
